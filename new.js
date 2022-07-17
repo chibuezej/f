eb4228c63a776573a21e4c313920ed118f1fc31a
chibuezej <Johnhenrychibueze@gmail.com>
2022-07-18 00:06:00 +0100

    first commit

---
+++ new.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var links = document.getElementById("myLinks");
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+  }
+
+  myFunction();
